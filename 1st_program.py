780e78121837e7754805fdbab099fbad37d99b50
Harsha Ninganna <121672013+HarshaNinganna@users.noreply.github.com>
2025-06-26 20:39:46 +0530

    Add files via upload

---
+++ 1st_program.py
@@ -0,0 +1,10 @@
+
+name = "harsha"
+age = 23
+is_student = True
+height = 5.6
+
+print(name)
+print(age)
+print(is_student)
+print(height)
---
+++ functionEx.py
@@ -0,0 +1,3 @@
+def display(name):
+    return f"hello,{name} !"
+print(display("Harsha"))
