c649b42acc980908cd43bf0eafb8c229d230dd21
Harsha Ninganna <121672013+HarshaNinganna@users.noreply.github.com>
2025-07-02 20:47:40 +0530

    Add files via upload

---
+++ Checkpolindrome.py
@@ -0,0 +1,14 @@
+def check_palindrome():
+    string1 = "sms"
+    string2 = "lms"  
+    if string1 == string1[::-1]:
+        print("string1 is a palindrome")
+    else:
+        print("string1 is not a palindrome") 
+    if string2 == string2[::-1]:
+        print("string2 is a palindrome")
+    else:
+        print("string2 is not a palindrome")
+check_palindrome()
+
+    
