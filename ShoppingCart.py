cbadce3f52d065edd4034e8adbce94b536c464c8
Harsha Ninganna <121672013+HarshaNinganna@users.noreply.github.com>
2025-07-03 20:56:19 +0530

    Add files via upload

---
+++ ShoppingCart.py
@@ -0,0 +1,6 @@
+shopping_cart = ["jeans", "cargo", "hoodie", "oversized t-shirt"]
+product = "cargo"
+if product in shopping_cart:
+    print(product, "is available in the cart.")
+else:
+    print(product, "is not in the cart.")
