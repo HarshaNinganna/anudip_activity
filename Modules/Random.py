f14bec9590eaaa9d62a851333ffb31cf07e75194
Harsha Ninganna <121672013+HarshaNinganna@users.noreply.github.com>
2025-08-01 14:37:50 +0530

    Add files via upload

---
+++ Modules/Random.py
@@ -0,0 +1,13 @@
+import random as rnd
+import math
+
+print("Random Integer (1-100):", rnd.randint(1, 100))
+
+sqroot_num = 49
+sqrt_val = math.sqrt(sqroot_num)
+print(f"Square root of {sqroot_num} is {sqrt_val}")
+
+angle_deg = 90
+angle_rad = math.radians(angle_deg)
+sine_val = math.sin(angle_rad)
+print(f"Sine of {angle_deg} degrees is {sine_val}")
