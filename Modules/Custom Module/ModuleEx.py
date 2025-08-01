569c561e0c68450a2fd0a466ab2f7d66a35739c5
Harsha Ninganna <121672013+HarshaNinganna@users.noreply.github.com>
2025-08-01 14:33:39 +0530

    Add files via upload

---
+++ Modules/Custom Module/ModuleEx.py
@@ -0,0 +1,13 @@
+from my_calculations import add, subtract
+from math import pi
+from math import log as logarithm
+ 
+print("Addition:", add(10, 5))         
+print("Subtraction:", subtract(10, 5))
+
+radius = 7
+area = pi * radius ** 2
+print("Area of circle:", area)
+
+log_value = logarithm(10)
+print("Natural log of 10:", log_value)
